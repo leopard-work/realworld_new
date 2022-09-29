7e37fa4fff72e072490e31441d00ccb2eb51804e
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2022-09-29 17:16:38 +0300

    визуализация banner

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import styles from '../../styles/Banner.module.scss';
 
 const Banner = ({ appName, token }) => {
   if (token) {
     return null;
   }
   return (
-    <div className="banner">
+    <div className={styles.banner}>
       <div className="container">
-        <h1 className="logo-font">
-          {appName.toLowerCase()}
-        </h1>
-        <p>test</p>
+        <h1 className="logo-font">{appName}</h1>
+        <h3>Место, где готовится новый опыт</h3>
       </div>
     </div>
   );
